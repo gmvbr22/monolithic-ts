f87a620b84d083a924f76dbc14820c924cd4dce7
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-28 18:58:09 -0400

    Add BcryptAdapter in Container

--- src/container.ts
+++ src/container.ts
@@ -1,9 +1,16 @@
 import 'reflect-metadata'
 import { Container } from 'inversify'
+import { SecretSymbol } from './application'
+import { BcryptAdapter } from './infrastructure'
 
 /**
  * Injeção de dependências
  */
 export async function initContainer () {
   const container = new Container()
+  //
+  // application/services
+  //
+  container.bind(SecretSymbol)
+    .toConstantValue(new BcryptAdapter(11))
 }
