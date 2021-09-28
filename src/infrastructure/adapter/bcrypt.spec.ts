117103f214660df71414a846e368c23af6fb1a6a
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-28 18:55:09 -0400

    Create bcrypt adapter

---
+++ src/infrastructure/adapter/bcrypt.spec.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata'
+import { BcryptAdapter } from './bcrypt'
+
+test('test bcrypt adapter', async () => {
+  const secret = new BcryptAdapter(10)
+  const hash = await secret.hash('1234')
+
+  expect(await secret.compare('123', hash)).toBeFalsy()
+  expect(await secret.compare('1234', hash)).toBeTruthy()
+})
---
+++ src/infrastructure/adapter/bcrypt.ts
@@ -0,0 +1,24 @@
+import { Secret } from '../../application'
+import { compareSync, hashSync } from 'bcrypt'
+import { injectable } from 'inversify'
+
+/**
+ * Adapter: Bcrypt
+ * Implementa: application/services/secret
+ */
+ @injectable()
+export class BcryptAdapter implements Secret {
+  private saltOrRounds: string | number
+
+  constructor (saltOrRounds: string | number) {
+    this.saltOrRounds = saltOrRounds
+  }
+
+  public async hash (password: string): Promise<string> {
+    return hashSync(password, this.saltOrRounds)
+  }
+
+  public async compare (password: string, hash: string): Promise<boolean> {
+    return compareSync(password, hash)
+  }
+}
