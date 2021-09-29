a497d29db00a633d42a15ddbae3a0d367a44725e
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-29 17:18:29 -0400

    Update BcryptAdapter

--- src/infrastructure/adapter/bcrypt.spec.ts
+++ src/infrastructure/adapter/bcrypt.spec.ts
@@ -1,9 +1,9 @@
 import { BcryptAdapter } from './bcrypt'
 
-test('test bcrypt adapter', async () => {
+test('Adapter: BcryptAdapter', async () => {
   const secret = new BcryptAdapter(10)
-  const hash = await secret.hash('1234')
+  const hash = await secret.hash('admin')
 
-  expect(await secret.compare('123', hash)).toBeFalsy()
-  expect(await secret.compare('1234', hash)).toBeTruthy()
+  expect(await secret.compare('user', hash)).toBeFalsy()
+  expect(await secret.compare('admin', hash)).toBeTruthy()
 })
--- src/infrastructure/adapter/bcrypt.ts
+++ src/infrastructure/adapter/bcrypt.ts
@@ -1,14 +1,16 @@
+import assert from 'assert'
 import { compareSync, hashSync } from 'bcrypt'
 import { HashComparator, Hasher } from '../../application'
 
-/**
- * Adapter: Bcrypt
- */
 export class BcryptAdapter implements Hasher, HashComparator {
   private saltOrRounds: string | number
 
   constructor (saltOrRounds: string | number) {
     this.saltOrRounds = saltOrRounds
+    assert(
+      typeof this.saltOrRounds === 'string' ||
+      typeof this.saltOrRounds === 'number'
+    )
   }
 
   public async hash (password: string): Promise<string> {
