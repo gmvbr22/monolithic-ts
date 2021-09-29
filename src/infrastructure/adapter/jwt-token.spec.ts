9b03c1684d1eae0546d06d1c9eb8e8ee7b009eff
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-29 17:16:39 -0400

    Create JWTAdapter

---
+++ src/infrastructure/adapter/jwt-token.spec.ts
@@ -0,0 +1,18 @@
+import { JWTAdapter } from './jwt-token'
+
+test('Adapter: JWTAdapter', async () => {
+  const jwt = new JWTAdapter('password', 10)
+
+  const token = await jwt.create({
+    sub: 'admin user'
+  })
+  expect(typeof token.expire).toBe('number')
+  expect(typeof token.token).toBe('string')
+
+  const invalid = await jwt.validate('xxx')
+  expect(invalid).toBeNull()
+
+  const valid = await jwt.validate<{ sub: string }>(token.token)
+  expect(valid).not.toBeNull()
+  expect(valid.sub).toBe('admin user')
+})
---
+++ src/infrastructure/adapter/jwt-token.ts
@@ -0,0 +1,34 @@
+import assert from 'assert'
+import { sign, verify } from 'jsonwebtoken'
+import { Token, TokenResult, TokenValidation } from '../../application'
+
+export class JWTAdapter implements Token, TokenValidation {
+  private secret: string
+  private expire: number
+
+  constructor (secret: string, expire: number) {
+    this.secret = secret
+    this.expire = expire
+
+    assert(typeof this.secret === 'string')
+    assert(typeof this.expire === 'number')
+  }
+
+  public async create (payload: any): Promise<TokenResult> {
+    const expireIn = Math.floor(Date.now() / 1000) + this.expire
+    const data = Object.assign(payload, { exp: expireIn })
+    const token = sign(data, this.secret)
+    return {
+      expire: expireIn,
+      token
+    }
+  }
+
+  public async validate<T> (token: string): Promise<T|null> {
+    try {
+      return verify(token, this.secret) as T
+    } catch (err) {
+      return null
+    }
+  }
+}
