88079f5e126661302d66ccaf70adc403af979a67
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-30 12:55:31 -0400

    Update paths from app.ts

--- src/infrastructure/app.ts
+++ src/infrastructure/app.ts
@@ -1,6 +1,6 @@
-import { Config } from './config'
-import { BcryptAdapter, JWTAdapter } from './adapter'
-import { MongoConnection } from './db'
+import { Config } from '@infra/config'
+import { MongoConnection, UserRepository } from '@infra/db'
+import { BcryptAdapter, JWTAdapter, PinoAdapter } from '@infra/adapter'
 
 export class Application {
   private config: Config
@@ -13,7 +13,11 @@ export class Application {
     const bcryptAdapter = new BcryptAdapter(this.config.passwordRounds)
     const jwtAdapter = new JWTAdapter(this.config.tokenSecret, this.config.tokenExpire)
 
+    const logger = new PinoAdapter()
+
     const mongodb = new MongoConnection(this.config.mongoUrl)
     await mongodb.connect()
+
+    const userRepository = new UserRepository(mongodb, logger, 'user')
   }
 }
