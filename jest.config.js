d82af5cce46072f628e1e81127f3dd6ca3942b5a
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-30 11:30:25 -0400

    Config typescript paths

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,10 @@ const { defaults: tsjPreset } = require('ts-jest/presets')
 module.exports = {
   preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
-  transform: tsjPreset.transform
+  transform: tsjPreset.transform,
+  globals: {
+    'ts-jest': {
+      compiler: 'ttypescript'
+    }
+  }
 }
