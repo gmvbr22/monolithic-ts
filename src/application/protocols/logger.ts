212b90676ca98fe8f3cedd84ba1599821c4a750e
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-30 10:15:39 -0400

    Create Logger

---
+++ src/application/protocols/logger.ts
@@ -0,0 +1,10 @@
+type LogFn = (message: string, ...args: unknown[]) => void;
+
+export interface Logger {
+  debug: LogFn;
+  info: LogFn;
+  warn: LogFn;
+  error: LogFn;
+  fatal: LogFn;
+  trace: LogFn;
+}
---
+++ src/infrastructure/adapter/pino-logger.ts
@@ -0,0 +1,34 @@
+import Pino from 'pino'
+import { Logger } from '../../application'
+
+export class PinoAdapter implements Logger {
+  private pino: Pino.Logger
+
+  constructor () {
+    this.pino = Pino()
+  }
+
+  public debug (message: string, ...args: unknown[]) {
+    return this.pino.debug(message, ...args)
+  }
+
+  public info (message: string, ...args: unknown[]) {
+    return this.pino.info(message, ...args)
+  }
+
+  public warn (message: string, ...args: unknown[]) {
+    return this.pino.warn(message, ...args)
+  }
+
+  public error (message: string, ...args: unknown[]) {
+    return this.pino.error(message, ...args)
+  }
+
+  public fatal (message: string, ...args: unknown[]) {
+    return this.pino.fatal(message, ...args)
+  }
+
+  public trace (message: string, ...args: unknown[]) {
+    return this.pino.trace(message, ...args)
+  }
+}
