1ead7dcf31a2dcf557406274b6562885389d5223
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-30 06:06:10 -0400

    Update Config

--- src/infrastructure/config.ts
+++ src/infrastructure/config.ts
@@ -6,33 +6,28 @@ export class Config {
   public tokenSecret: string;
   public tokenExpire: number;
   public passwordRounds: number
+  public mongoUrl: string
 
   constructor () {
     const env = envSchema({
       schema: {
         type: 'object',
         properties: {
-          HOST: {
-            type: 'string',
-            default: '0.0.0.0'
-          },
-          PORT: {
-            type: 'number',
-            default: 8080
-          },
-          TOKEN_SECRET: {
-            type: 'string'
-          },
-          TOKEN_EXPIRE: {
-            type: 'number',
-            default: 3600
-          },
-          PASSWORD_ROUNDS: {
-            type: 'number',
-            default: 11
-          }
+          HOST: { type: 'string', default: '0.0.0.0' },
+          PORT: { type: 'number', default: 8080 },
+          TOKEN_SECRET: { type: 'string' },
+          TOKEN_EXPIRE: { type: 'number', default: 3600 },
+          PASSWORD_ROUNDS: { type: 'number', default: 11 },
+          MONGO_URL: { type: 'string' }
         },
-        required: ['HOST', 'PORT', 'TOKEN_SECRET', 'TOKEN_EXPIRE', 'PASSWORD_ROUNDS']
+        required: [
+          'HOST',
+          'PORT',
+          'TOKEN_SECRET',
+          'TOKEN_EXPIRE',
+          'PASSWORD_ROUNDS',
+          'MONGO_URL'
+        ]
       }
     })
     this.serverPort = env.PORT as number
@@ -40,5 +35,6 @@ export class Config {
     this.tokenSecret = env.TOKEN_SECRET as string
     this.tokenExpire = env.TOKEN_EXPIRE as number
     this.passwordRounds = env.PASSWORD_ROUNDS as number
+    this.mongoUrl = env.MONGO_URL as string
   }
 }
