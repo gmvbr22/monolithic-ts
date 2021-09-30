ab689cc14d605950e45de41885792fafd0d1a24f
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-30 09:38:51 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-
 const { defaults: tsjPreset } = require('ts-jest/presets')
 
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
