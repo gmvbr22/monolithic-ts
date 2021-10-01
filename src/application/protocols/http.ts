9fa7355eb7a0e5b7995aba79e7ba40bd3ba59f59
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-10-01 19:25:44 -0400

    Update http protocol

--- src/application/protocols/http.ts
+++ src/application/protocols/http.ts
@@ -1,9 +1,23 @@
-export interface HTTPRequest<Body = {}, Headers = {}> {
-  body: Body,
-  headers: Headers
+export type HTTPBody = unknown
+export type HTTPHeader = unknown
+
+export interface HTTPRequestType {
+  Body?: HTTPBody
+  Headers?: HTTPHeader
+}
+
+export interface HTTPRequest<H extends HTTPRequestType = HTTPRequestType> {
+  body: H['Body'],
+  headers: H['Headers']
 }
 
 export interface HTTPReply {
   code: (code: number) => HTTPReply
   send: (payload: any) => HTTPReply
+}
+
+export interface HTTPError {
+  success: false,
+  errorCode: number,
+  errorMessage: string
 }
