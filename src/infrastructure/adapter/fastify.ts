4a47f9690117eda8df53e880c392fa865a50adbb
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-10-02 07:44:44 -0400

    Setup fastify adapter

---
+++ src/infrastructure/adapter/fastify.ts
@@ -0,0 +1,29 @@
+import { HttpFramework, HttpRouteList } from '@app'
+import fastify, { FastifyInstance } from 'fastify'
+import { injectable } from 'inversify'
+
+@injectable()
+export class FastifyServer implements HttpFramework {
+  private fastify: FastifyInstance
+
+  public constructor () {
+    this.fastify = fastify()
+  }
+
+  public registry (list: HttpRouteList) {
+    for (const route of list) {
+      this.fastify.route({
+        url: route.url,
+        method: route.method,
+        schema: {
+          body: route.schema.body
+        },
+        handler: async (req, res) => route.controller(req, res)
+      })
+    }
+  }
+
+  public async listen (port: number, host:string) {
+    return this.fastify.listen(port, host)
+  }
+}
