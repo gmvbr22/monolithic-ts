e63b4397d9bff91e0b58bae8c962d07e19d9c88e
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-10-02 07:44:28 -0400

    Create auth routes

---
+++ src/interface/routes/auth.ts
@@ -0,0 +1,29 @@
+import { injectable } from 'inversify'
+import { HttpRouteList } from '@app'
+import { AuthController } from '@interface/controller'
+import { LoginSchema } from '@interface/schema'
+
+@injectable()
+export class AuthRoutes {
+  private authController: AuthController
+
+  public constructor (authController: AuthController) {
+    this.authController = authController
+  }
+
+  /**
+   * Define as rotas utilizadas em Auth
+   */
+  public initialize (): HttpRouteList {
+    return [
+      {
+        method: 'POST',
+        url: '/login',
+        schema: {
+          body: LoginSchema
+        },
+        controller: this.authController.login
+      }
+    ]
+  }
+}
